cf37462a44c0143a058ead292f576170dd38cdea
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-11-04 17:57:09 +0100

    implemented a route component

---
+++ src/routes/routes.js
@@ -0,0 +1,20 @@
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import Loading from "../components/loading";
+
+const Home = lazy(() => import("../pages/home"));
+const NotFoundPage = lazy(() => import("../pages/404"));
+const ErrFallBack = lazy(() => import("../pages/errorBoundary"));
+
+function RouteApp() {
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pages/404" element={<NotFoundPage />} />
+        <Route path="/pages/errorBoundary" element={<ErrFallBack />} />
+      </Routes>
+    </Suspense>
+  );
+}
+export default RouteApp;
