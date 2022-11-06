077458977fa77e622686a22878873ee92ff92234
okhuarobo osayuki <osayukiraymond@gmail.com>
2022-11-06 13:07:41 +0100

    implemented ErrorBoundary on the whole app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./assets/style.css";
+import "./assets/errorBoundary.css";
 import RouteApp from "./routes/routes";
 import Loading from "./components/loading";
 import { useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  return (
+    <div role="alert" className="errFallBack">
+      <div className="container">
+        <h1 className="errFallBack_para" style={{ color: "red" }}>OOPs! Something went wrong</h1>
+        <pre >{error.message}</pre>
+        <button onClick={resetErrorBoundary}>Reset</button>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -10,7 +24,17 @@ function App() {
     setLoading(false);
   }, 5000);
 
-  return <>{loading ? <Loading /> : <RouteApp />}</>;
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <RouteApp />
+        </ErrorBoundary>
+      )}
+    </>
+  );
 }
 
 export default App;
