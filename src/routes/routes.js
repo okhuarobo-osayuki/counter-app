e3bf92cd0788812b3756ad07f90b5662a06e0d00
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-05 13:04:39 +0100

    created a nested route on Home route, adding the separate counter pages

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,6 +6,8 @@ import Loading from "../components/loading";
 const Home = lazy(() => import("../pages/home"));
 const NotFoundPage = lazy(() => import("../pages/404"));
 const ErrFallBack = lazy(() => import("../pages/errorBoundary"));
+const CounterWithReducer = lazy(() => import("../pages/CounterWithReducer"));
+const CounterWithCustomHook = lazy(() => import("../pages/CounterWithCustomHook"));
 
 function RouteApp() {
   return (
@@ -13,7 +15,10 @@ function RouteApp() {
       <NavBar />
       <Suspense fallback={<Loading />}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />}>
+            <Route path="/pages/CounterWithReducer" element={<CounterWithReducer/>} />
+            <Route path="/pages/CounterWithCustomHook" element={<CounterWithCustomHook/>} />
+          </Route>
           <Route path="/pages/404" element={<NotFoundPage />} />
           <Route path="/pages/errorBoundary" element={<ErrFallBack />} />
           <Route path="*" element={<NotFoundPage />} />
