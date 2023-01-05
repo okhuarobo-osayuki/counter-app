73f27d57344ba0cb95f1f9bd353feff6c37536aa
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-05 13:05:50 +0100

    exported NavBarLink to make it reuseable globally

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,13 @@ import "../assets/css/navbar.css";
 
 //A custom link component that styles itself as active if the pathname of the URL matches the active link
 
-function NavBarLink({ to, children, ...props }) {
+export function NavBarLink({ to, children, className, ...props }) {
   const resolvedPath = useResolvedPath(to);
   const isActive = useMatch({ path: resolvedPath.pathname });
 
   return (
     <li className={isActive ? "active" : "nav-item"}>
-      <NavLink to={to} {...props}>
+      <NavLink to={to} className={className} {...props}>
         {children}
       </NavLink>
     </li>
