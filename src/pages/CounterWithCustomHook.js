dd2fb25782d0e5e9fa9b472b3d9c1e5c57700d47
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-05 13:02:49 +0100

    created a separate page for a count powered by a custom useCounter hook

---
+++ src/pages/CounterWithCustomHook.js
@@ -0,0 +1,35 @@
+import "../assets/css/style.css";
+import useCounter from "../hooks/useCounter";
+
+function CounterWithCustomHook() {
+  const [count, increment, decrement, reset, setValue, inputRef] =
+    useCounter();
+
+  return (
+    <main className="content">
+      <p className="counter-value">Count: {count}</p>
+      <div className="set-value-input">
+        <input
+          ref={inputRef}
+          type={"number"}
+          placeholder={"Enter number here"}
+          name="value"
+        />
+        <button onClick={setValue}>Set Value</button>
+      </div>
+      <div className="buttons">
+        <button className="btn" onClick={increment}>
+          Increase
+        </button>
+        <button className="btn" onClick={decrement}>
+          Decrease
+        </button>
+        <button className="btn reset" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default CounterWithCustomHook;
