cf4fa8aa7414d4e96c119cba01d9ab6576fb0a97
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-05 01:54:00 +0100

    added Counter App logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import { NavLink, useMatch, useResolvedPath } from "react-router-dom";
+import { Link, NavLink, useMatch, useResolvedPath } from "react-router-dom";
 import "../assets/css/navbar.css";
 
 //A custom link component that styles itself as active if the pathname of the URL matches the active link
@@ -7,25 +7,28 @@ function NavBarLink({ to, children, ...props }) {
   const resolvedPath = useResolvedPath(to);
   const isActive = useMatch({ path: resolvedPath.pathname });
 
-  return(
+  return (
     <li className={isActive ? "active" : "nav-item"}>
-        <NavLink to={to} {...props}>
-            {children}
-        </NavLink>
+      <NavLink to={to} {...props}>
+        {children}
+      </NavLink>
     </li>
-  )
+  );
 }
 
 function NavBar() {
-    return(
-        <nav>
-            <ul>
-                <NavBarLink to="/">Home</NavBarLink>
-                <NavBarLink to="/pages/404">404 Page</NavBarLink>
-                <NavBarLink to="/pages/errorBoundary">Error Boundary</NavBarLink>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <Link to="/" className="logo">
+        Counter App
+      </Link>
+      <ul>
+        <NavBarLink to="/">Home</NavBarLink>
+        <NavBarLink to="/pages/404">404 Page</NavBarLink>
+        <NavBarLink to="/pages/errorBoundary">Error Boundary</NavBarLink>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
