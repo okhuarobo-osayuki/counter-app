d7312116366397107c8d514074e22c82150a4e41
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-05 13:11:22 +0100

    lazy imported the counter pages wrapping them around a Suspense

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,37 @@
+import { lazy } from "react";
 import { Helmet } from "react-helmet-async";
+import { Outlet } from "react-router-dom";
 import "../assets/css/style.css";
-import useCounter from "../hooks/useCounter";
+import { NavBarLink } from "../components/header";
+
+const CounterWithReducer = lazy(() => import("./CounterWithReducer"));
+const CounterWithCustomHook = lazy(() => import("./CounterWithCustomHook"));
 
 function Home() {
-  const [state, increment, decrement, reset, setValue, inputRef] = useCounter();
+  // const [state, increment, decrement, reset, setValue, inputRef] = useCounter();
   return (
     <>
-    <Helmet>
-      <title>Home</title>
-      <meta name="description" content="Counter App using React hooks and custom hooks to increase, decrease, reset and set value of the count." />
-      <link rel="canonical" href="/home" />
-    </Helmet>
-    <div className="home">
+      <Helmet>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="Counter App using React hooks and custom hooks to increase, decrease, reset and set value of the count."
+        />
+        <link rel="canonical" href="/home" />
+      </Helmet>
+      <section className="child-nav">
+        <NavBarLink
+          to={"/pages/CounterWithReducer"}
+          element={<CounterWithReducer />}
+          className="child-nav-link"
+        >
+          Counter with Reducer
+        </NavBarLink>
+        <NavBarLink to={"/pages/CounterWithCustomHook"} element={<CounterWithCustomHook />} className="child-nav-link">
+          Counter with Custom Hook
+        </NavBarLink>
+      </section>
+      {/* <div className="home">
       <main className="content">
         <p className="counter-value">Count: {state.count}</p>
         <div className="set-value-input">
@@ -24,7 +44,8 @@ function Home() {
           <button className="btn reset" onClick={reset}>Reset</button>
         </div>
       </main>
-    </div>
+    </div> */}
+      <Outlet />
     </>
   );
 }
