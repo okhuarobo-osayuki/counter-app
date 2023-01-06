d916acc594ed092d5935b41c633d780f2420b24b
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-06 23:21:36 +0100

    removed NavBar from route file to ensure it does not appear globally

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "../components/header";
 import Loading from "../components/loading";
+import Signup from "../pages/signup";
+import Login from "../pages/login";
 
 const Home = lazy(() => import("../pages/home"));
 const NotFoundPage = lazy(() => import("../pages/404"));
@@ -12,13 +13,14 @@ const CounterWithCustomHook = lazy(() => import("../pages/CounterWithCustomHook"
 function RouteApp() {
   return (
     <>
-      <NavBar />
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Home />}>
             <Route path="/pages/CounterWithReducer" element={<CounterWithReducer/>} />
             <Route path="/pages/CounterWithCustomHook" element={<CounterWithCustomHook/>} />
           </Route>
+          <Route path="/pages/signup" element={<Signup />} />
+          <Route path="/pages/login" element={<Login />} />
           <Route path="/pages/404" element={<NotFoundPage />} />
           <Route path="/pages/errorBoundary" element={<ErrFallBack />} />
           <Route path="*" element={<NotFoundPage />} />
