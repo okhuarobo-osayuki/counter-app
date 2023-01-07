a486ac97abc81b74716b8d39eb53f477e53f3cb6
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-07 10:52:37 +0100

    added logout button & user email render

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, NavLink, useMatch, useResolvedPath } from "react-router-dom";
 import "../assets/css/navbar.css";
 
@@ -17,6 +18,16 @@ export function NavBarLink({ to, children, className, ...props }) {
 }
 
 function NavBar() {
+  const [error, setError] = useState("");
+
+  const handleLogout = () => {
+    setError("");
+    try {
+      // logout(auth);
+    } catch {
+      setError("Failed to log out");
+    }
+  };
   return (
     <nav>
       <Link to="/" className="logo">
@@ -27,6 +38,13 @@ function NavBar() {
         <NavBarLink to="/pages/404">404 Page</NavBarLink>
         <NavBarLink to="/pages/errorBoundary">Error Boundary</NavBarLink>
       </ul>
+      <div>
+        <p className="user">Hi </p>
+        <button className="logout" onClick={handleLogout}>
+          Log out
+        </button>
+      </div>
+      {error && <p className="error">{error}</p>}
     </nav>
   );
 }
