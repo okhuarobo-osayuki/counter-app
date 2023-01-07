36f9bd770143f5602be7aa7e96ee6b7dd9175109
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-07 12:19:03 +0100

    modified

--- src/assets/config/firebase.js
+++ src/assets/config/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+   apiKey: "AIzaSyA-Z05QpISbCGY9D2cKljDfJi50o8GqPzY",
+  authDomain: "counter-app-development.firebaseapp.com",
+  projectId: "counter-app-development",
+  storageBucket: "counter-app-development.appspot.com",
+  messagingSenderId: "669970570923",
+  appId: "1:669970570923:web:284d346fec94217842a1ee"
 };
 
 // Initialize Firebase
