8564c8dababa573316171cc6e32b13024c2ca941
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-07 12:13:33 +0100

    added an expression that shows currentUser email as a name; addeda function that navigates to login page when logged out

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import { Link, NavLink, useMatch, useResolvedPath } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, NavLink, useMatch, useNavigate, useResolvedPath } from "react-router-dom";
 import "../assets/css/navbar.css";
+import { AuthContext } from "../context/authContext";
 
 //A custom link component that styles itself as active if the pathname of the URL matches the active link
 
@@ -19,15 +20,20 @@ export function NavBarLink({ to, children, className, ...props }) {
 
 function NavBar() {
   const [error, setError] = useState("");
+  const { currentUser, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const handleLogout = () => {
+  async function handleLogout() {
     setError("");
+
     try {
-      // logout(auth);
+      await logout();
+      navigate("/pages/login");
     } catch {
       setError("Failed to log out");
     }
-  };
+  }
+
   return (
     <nav>
       <Link to="/" className="logo">
@@ -39,7 +45,9 @@ function NavBar() {
         <NavBarLink to="/pages/errorBoundary">Error Boundary</NavBarLink>
       </ul>
       <div>
-        <p className="user">Hi </p>
+        <p className="user">
+          Hi <span>{currentUser.email}</span>!
+        </p>
         <button className="logout" onClick={handleLogout}>
           Log out
         </button>
