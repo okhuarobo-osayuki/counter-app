845361733c85cf4895824efea0b7b0cebaafd85c
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-07 12:42:31 +0100

    did a conditional rendering of routes if there is a currentUser

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,31 +1,51 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Loading from "../components/loading";
 import Signup from "../pages/signup";
 import Login from "../pages/login";
+import { AuthContext } from "../context/authContext";
 
 const Home = lazy(() => import("../pages/home"));
 const NotFoundPage = lazy(() => import("../pages/404"));
 const ErrFallBack = lazy(() => import("../pages/errorBoundary"));
 const CounterWithReducer = lazy(() => import("../pages/CounterWithReducer"));
-const CounterWithCustomHook = lazy(() => import("../pages/CounterWithCustomHook"));
+const CounterWithCustomHook = lazy(() =>
+  import("../pages/CounterWithCustomHook")
+);
 
 function RouteApp() {
+  const { currentUser } = useContext(AuthContext);
   return (
     <>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            <Route path="/pages/CounterWithReducer" element={<CounterWithReducer/>} />
-            <Route path="/pages/CounterWithCustomHook" element={<CounterWithCustomHook/>} />
-          </Route>
-          <Route path="/pages/signup" element={<Signup />} />
-          <Route path="/pages/login" element={<Login />} />
-          <Route path="/pages/404" element={<NotFoundPage />} />
-          <Route path="/pages/errorBoundary" element={<ErrFallBack />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
+      {currentUser ? (
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route
+                path="/pages/CounterWithReducer"
+                element={<CounterWithReducer />}
+              />
+              <Route
+                path="/pages/CounterWithCustomHook"
+                element={<CounterWithCustomHook />}
+              />
+            </Route>
+            <Route path="/pages/signup" element={<Signup />} />
+            <Route path="/pages/login" element={<Login />} />
+            <Route path="/pages/404" element={<NotFoundPage />} />
+            <Route path="/pages/errorBoundary" element={<ErrFallBack />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      ) : (
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/pages/signup" element={<Signup />} />
+            <Route path="/pages/login" element={<Login />} />
+            <Route path="*" element={<Login />} />
+          </Routes>
+        </Suspense>
+      )}
     </>
   );
 }
