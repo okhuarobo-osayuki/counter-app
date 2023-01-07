83112930e09cd031b07a4bf8e6fe30dff29e1662
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-01-07 23:14:20 +0100

    modified

--- src/assets/config/firebase.js
+++ src/assets/config/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import "firebase/auth";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
